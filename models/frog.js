944962aa1d311faa2275fddcf5152c5cb5e15c9d
Elizabeth Koning <koningelizabeth@gmail.com>
2019-04-23 22:28:07 -0400

    Added basic frog mobility

--- models/frog.js
+++ models/frog.js
@@ -1,3 +1,8 @@
+var mesh; // uses hacky fix to make frog mobile
+// TODO: remove this hacky fix
+
+// fix to make frog mobile from: https://discourse.threejs.org/t/how-to-move-car-obj-file-on-map-solved/1173
+
 class Frog {
 
   // model from: https://sketchfab.com/3d-models/poison-dart-frog-97142ccef8c74650b34e0547b2812c2b
@@ -6,16 +11,18 @@ class Frog {
   constructor(scene, x, y, z) {
     this.scene = scene;
 
-    // var geo = new THREE.BoxBufferGeometry(50, 50, 50);
-    // var mat = new THREE.MeshLambertMaterial( { color: 0x0B6623 } );
-    // this.mesh = new THREE.Mesh(geo, mat);
-    // this.setLocation(x, y, z);
-    // scene.add(this.mesh);
+    // frog model is a child of this mesh. It moves with the mesh.
+    var geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( );
+    this.mesh = new THREE.Mesh( geometry, material );
+    mesh = this.mesh; // this is the hacky line
+    scene.add( this.mesh );
 
-    this.load_example();
+    this.load_example('models/gltf/poison_dart_frog/scene.gltf');
+    this.setLocation(x, y, z);
   }
 
-  load_example() {
+  load_example(url) {
     // loading from example at: https://threejs.org/docs/#examples/loaders/GLTFLoader
     // Instantiate a loader
     var loader = new THREE.GLTFLoader();
@@ -23,43 +30,34 @@ class Frog {
     // Load a glTF resource
     loader.load(
       // resource URL
-      'models/gltf/poison_dart_frog/scene.gltf',
+      url,
       // called when the resource is loaded
       function ( gltf ) {
 
+        gltf.scene.position.set(-43, 0, 255);
         scene.add( gltf.scene );
-
-        gltf.animations; // Array<THREE.AnimationClip>
-        gltf.scene; // THREE.Scene
-        gltf.scenes; // Array<THREE.Scene>
-        gltf.cameras; // Array<THREE.Camera>
-        gltf.asset; // Object
-
-      },
-      // called while loading is progressing
-      function ( xhr ) {
-
-        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-      },
-      // called when loading has errors
-      function ( error ) {
-
-        console.log( 'An error happened' );
-
+        mesh.add(gltf.scene);
       }
     );
+
   }
 
   moveBy(x, y, z) {
-    // TODO: move by distance
     // TODO: animate movement
+
+    var x_coord = this.mesh.position.x;
+    var y_coord = this.mesh.position.y;
+    var z_coord = this.mesh.position.z;
+    this.setLocation(x+x_coord, y+y_coord, z+z_coord);
   }
 
   setLocation(x, y, z) {
-    // this.mesh.position.set(x, y, z);
-    // TODO: move base of item to ground level
-    // TODO: move object to location
+    this.mesh.position.set(x, y, z);
+  }
+
+  setRotation(angle) {
+    // rotates around the center of the frog
+    this.mesh.rotation.y = angle;
   }
 
   // TODO: add collision detection
