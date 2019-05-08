d9651ecb4a5d1e38a7fa4571852ade69a551e502
sethgalyon <seth.galyon@gmail.com>
2019-05-07 21:10:22 -0400

    Added bush collision

--- models/hedge.js
+++ models/hedge.js
@@ -1,20 +1,23 @@
 class Hedge {
-  constructor(scene, minX, minZ, maxX, maxZ, xSpace, zSpace) {
+  constructor(scene, minX, minZ, maxX, maxZ, xSpace, zSpace, hedgeList) {
     this.scene = scene;
     this.border = [];
 
     for(var z = minZ; z < maxZ; z += zSpace) {
-      this.border.push(new Border(scene, maxX, 0, z));
+      this.border.push(new Border(scene, maxX, 0, z, minX, minZ, maxX, maxZ));
     }
     for(var z = minZ; z < maxZ; z += zSpace) {
-      this.border.push(new Border(scene, minX, 0, z));
+      this.border.push(new Border(scene, minX, 0, z, minX, minZ, maxX, maxZ));
     }
     for(var x = minX; x < maxX; x += xSpace) {
-      this.border.push(new Border(scene, x, 0, maxZ));
+      this.border.push(new Border(scene, x, 0, maxZ, minX, minZ, maxX, maxZ));
     }
     for(var x = minX; x < maxX; x += xSpace) {
-      this.border.push(new Border(scene, x, 0, minZ));
+      this.border.push(new Border(scene, x, 0, minZ, minX, minZ, maxX, maxZ));
     }
 
+    for(var i = 0; i < this.border.length; i++) {
+      hedgeList.push( this.border[i].mesh );
+    }
   }
 }
